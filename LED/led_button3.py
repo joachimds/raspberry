de7dd99208731d0603f5637d8c68c0861fa15f85
joachim de schrijver <jds@joachimdeschrijver.be>
2017-11-30 21:22:52 +0100

    led 3

--- LED/led_button3.py
+++ LED/led_button3.py
@@ -1,4 +1,4 @@
-from gpiozero import LED, Button
+from gpiozero import LED, Button, Buzzer
 from time import sleep
 from signal import pause
 
@@ -6,29 +6,38 @@ greenpin = 26
 orangepin = 19
 redpin = 13
 buttonpin = 2
+buzzerpin = 20
 
-greentime = 10
-orangetime = 2
-redtime = 10
+greentime = 5
+orangetime = 1
+redtime = 5
 
-delaytime = 5
+delaytime = 2
 
 green = LED(greenpin)
 orange = LED(orangepin)
 red = LED(redpin)
 button = Button(buttonpin)
+buzzer = Buzzer(buzzerpin)
 
 while True:
-    
     red.on()
     button.wait_for_press()
+    buzzer.beep()
+    buzzer.beep()
+    buzzer.off()
     sleep(delaytime)
     red.off()
     green.on()
+    buzzer.on()
     sleep(greentime)
+    buzzer.off()
     green.off()
     orange.on()
     sleep(orangetime)
+    buzzer.beep()
+    buzzer.beep()
+    buzzer.off()
     orange.off()
     red.on()
     sleep(redtime)
